9b2de88a2608b4015338f2ca95262208c0e6b24f
NerdyJ23 <surefiremooly@gmail.com>
2023-06-24 19:26:40 +1000

    Initial Commit with steamcmd

---
+++ Steam/SteamConsole.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SteamGameArchiver.Steam
+{
+    internal class SteamConsole
+    {
+        const string CMD_PATH = "./steamcmd.exe";
+
+        static void Main(string[] args)
+        {
+
+        }
+
+        public void GetAppList()
+        {
+
+        }
+    }
+}
